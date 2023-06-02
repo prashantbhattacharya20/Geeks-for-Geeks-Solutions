827ec468214ab4146f509aa1e105ff9c6c124714
Prashant Bhattacharya <103065549+prashantbhattacharya20@users.noreply.github.com>
2023-06-02 20:28:57 +0530

    Added Solution - GfG to GitHub

---
+++ School/Odd or Even/odd-or-even.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.oddEven(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String oddEven(int N){
+        String result = "";
+        if(N % 2 == 0) {
+            result = "even";
+        } else if (N % 2 != 0) {
+            result = "odd";
+        }
+        
+        return result;
+    }
+}
